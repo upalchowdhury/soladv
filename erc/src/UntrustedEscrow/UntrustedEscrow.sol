35c620677765f68234ceba38de5759721bab7ffc
upalchowdhury <upal.chowdhury@outlook.com>
2023-05-02 08:01:53 -0400

    fixed escrow

--- erc/src/UntrustedEscrow/UntrustedEscrow.sol
+++ erc/src/UntrustedEscrow/UntrustedEscrow.sol
@@ -4,39 +4,22 @@ import "@openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin-contracts/contracts/utils/math/SafeMath.sol";
 import "solmate/src/utils/ReentrancyGuard.sol";
 import "@openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 
 
-
-
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract SaferUntrustedEscrow is ReentrancyGuard {
     using SafeMath for uint256;
 
-
-============================================= follow below =====================
-
-
-
-
-contract SaferUntrustedEscrow is IERC20, ERC20, ReentrancyGuard {
-    using SafeMath for uint256;
-    using SafeERC20 for IERC20;
-
-
     address public immutable buyer;
     address payable public immutable seller;
-    uint public immutable amount;
+    uint private amount;
     uint public immutable releaseTime;
     bool public released = false;
-    IERC20 public token;
+    //ERC20 public token;
     
-    constructor(address _buyer, address payable _seller, uint _amount, address _token) payable {
+    constructor(address _buyer, address payable _seller, uint _amount) payable {
         require(_buyer != address(0), "Invalid buyer address");
         require(_seller != address(0), "Invalid seller address");
         require(msg.value == _amount, "Insufficient funds");
@@ -44,11 +27,19 @@ contract SaferUntrustedEscrow is IERC20, ERC20, ReentrancyGuard {
         seller = _seller;
         amount = _amount;
         releaseTime = block.timestamp + 3 days;
-        token = IERC20(_token);
+    
 
     }
 
-    function deposit(ERC20 token,uint256 _amount) external nonReentrant {
+
+    
+    modifier onlySeller() {
+        require(msg.sender == seller,'not seller');
+        _;
+    }
+
+    function deposit(address tokenAddress,uint256 _amount) external payable nonReentrant() {
+        ERC20 token = ERC20(tokenAddress);
         require(msg.sender == buyer, "Only the buyer can deposit.");
         require(_amount > 0, "Amount must be greater than zero.");
         require(token.balanceOf(msg.sender) >= _amount, "Insufficient balance.");
@@ -57,20 +48,16 @@ contract SaferUntrustedEscrow is IERC20, ERC20, ReentrancyGuard {
         amount = amount.add(_amount);
     }
     
-    function release() public nonReentrant {
-        require(msg.sender == buyer, "Only the buyer can release the funds");
+    function release() public nonReentrant() onlySeller() {
+        require(msg.sender == seller, "Only the buyer can release the funds");
         require(releaseTime <= block.timestamp, "Release time has not yet been reached");
         require(!released, "Funds have already been released");
-        released = true;
         (bool success, ) = seller.call{value: amount}("");
         require(success, "Failed to send ether to seller");
+        released = true;
+
     }
-    
-    function cancel() public {
-        require(msg.sender == seller, "Only the seller can cancel the contract");
-        require(!released, "Funds have already been released");
-        selfdestruct(seller);
-    }
+
     
     function getBalance() public view returns (uint) {
         return address(this).balance;
