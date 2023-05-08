8312ea563fa298011b9afd036cc3dbc999b63804
upalchowdhury <upal.chowdhury@outlook.com>
2023-05-07 20:32:26 -0400

    added handler test

--- nft/test/nftStaking/Handler.sol
+++ nft/test/nftStaking/Handler.sol
@@ -7,5 +7,23 @@ import "../src/NftContract.sol";
 import "../src/NftStaking.sol";
 import "../src/RewardToken";
 
+import {CommonBase} from "forge-std/Base.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import {StdUtils} from "forge-std/StdUtils.sol";
 
+
+
+
+contract Handler is CommonBase, StdCheats, StdUtils {
+    DiscountedNFT public nft;
+
+    constructor(IERC20 token) {
+        token = IERC20(token);
+        deal(address(this), 10 ether);
+    }
+
+    function deposit(uint256 amount) public {
+        token.deposit{ value: amount }();
+    }
+}
 
